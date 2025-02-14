8a3936b28e903a526d52f61ba3a72be651503a5f
inda2220 <151499587+inda2220@users.noreply.github.com>
2025-02-14 08:25:45 -0700

    Update game.js

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -1,1 +1,8 @@
+var buttonColors = ["red", "blue", "green", "yellow"]
+var gamePattern = [];
 
+function nextSequence (){
+  let randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColors[randomNumber];
+  
+}
